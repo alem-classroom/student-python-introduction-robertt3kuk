fb89c0c866f1cd5e824151f3217b87fc25ce0852
biqontie <awesome.abaildaev@yandex.kz>
2022-04-10 15:23:20 +0600

    a

--- loops-for/forDict.py
+++ loops-for/forDict.py
@@ -1,4 +1,4 @@
 def reverse_dict(dict):
-    # swap keys and values within dict and return dict
-    dict = {value:key for key, value in dict.items()}
-    return dict 
+    # swap keys and values wtthin dict and return dict
+    dict = {value: key for key, value in dict.items()}
+    return dict
